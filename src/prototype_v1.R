b880b6c98f771c086b5e110c83f335bd179c374c
Jack Turek <jackturek@Jacks-MacBook-Pro.local>
2025-10-15 22:21:51 -0500

    Updating UTM to lat/long

--- src/prototype_v1.R
+++ src/prototype_v1.R
@@ -6,3 +6,9 @@
 library(tidyverse)
 library(leaflet)
 library(leaflet.extras)
+library(sp)
+library(sf)
+
+# Convert UTM to Lat/Long
+bexar_historical_markers <- bexar_historical_markers %>%
+  filter(!is.na(Utm_East) & !is.na(Utm_North))
